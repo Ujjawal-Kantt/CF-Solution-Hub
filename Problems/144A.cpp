1522db7645177d33046bd100205b7a73f514fa7d
Ujjawal-Kantt <uk1287@srmist.edu.in>
2024-11-06 15:03:49 +0530

    6-11-24

--- Problems/144A.cpp
+++ Problems/144A.cpp
@@ -1,22 +1,32 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
 int main()
 {
     int n;
-    cin >> n;
-    vector<int> lines(n);
-    for (int i = 0; i < n; i++)
+    std::cin >> n;
+    std::vector<int> heights(n);
+
+    for (int i = 0; i < n; ++i)
     {
-        cin >> lines[i];
+        std::cin >> heights[i];
     }
-    int max_index = 0;
-    for (int i = 0; i < n; i++)
+
+    int max_height = *std::max_element(heights.begin(), heights.end());
+    int min_height = *std::min_element(heights.begin(), heights.end());
+
+    int max_index = std::find(heights.begin(), heights.end(), max_height) - heights.begin();
+    int min_index = std::find(heights.rbegin(), heights.rend(), min_height) - heights.rbegin();
+    min_index = n - 1 - min_index;
+
+    int result = max_index + (n - 1 - min_index);
+    if (max_index > min_index)
     {
-        for (int j = i; j < n; j++)
-        {
-            if (lines[i] > lines[j])
-            {
-            }
-        }
+        result -= 1;
     }
+
+    std::cout << result << std::endl;
+
+    return 0;
 }
