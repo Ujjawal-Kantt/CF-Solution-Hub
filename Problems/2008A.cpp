3523af66bca171d48c2026d93098782175b83633
Ujjawal-Kantt <uk1287@srmist.edu.in>
2024-11-20 10:36:22 +0530

    20-11-24

--- Problems/2008A.cpp
+++ Problems/2008A.cpp
@@ -9,13 +9,13 @@ int main()
     {
         int a, b;
         cin >> a >> b;
-        if (a % 2 == 0 && a >= 2 * b)
+        if (a % 2 == 1 || (b % 2 == 1 && a < 2))
         {
-            cout << "YES" << endl;
+            cout << "NO" << endl;
         }
         else
         {
-            cout << "NO" << endl;
+            cout << "YES" << endl;
         }
     }
 
