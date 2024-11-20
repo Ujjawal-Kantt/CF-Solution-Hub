688cacb9810b774b2a05c1e94dab6e2fa3d41ea8
Ujjawal-Kantt <uk1287@srmist.edu.in>
2024-11-20 10:34:03 +0530

    20-11-24

---
+++ Problems/2008A.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a, b;
+        cin >> a >> b;
+        if (a % 2 == 0 && a >= 2 * b)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+
+    return 0;
+}
