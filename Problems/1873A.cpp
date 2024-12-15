8771ff4b00b8ecf1d423bb20f62549c544deaa87
Ujjawal-Kantt <uk1287@srmist.edu.in>
2024-12-15 14:49:53 +0530

    15-12-24

--- Problems/1873A.cpp
+++ Problems/1873A.cpp
@@ -1,22 +1,23 @@
-// #include <bits/stdc++.h>
-// using namespace std;
-// int main()
-// {
-//     int t;
-//     cin >> t;
-//     while (t--)
-//     {
-//         string s;
-//         cin >> s;
-//         unordered_map<char, int> mpp;
-//         for (int i = 0; i < 3; i++)
-//         {
-//             mpp[s[i]] = i;
-//         }
-//         for (auto val : s)
-//         {
-//             if(s[])
-
-//         }
-//     }
-// }
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string s;
+        cin >> s;
+        int ans = 0;
+        string temp = "abc";
+        for (int i = 0; i < 3; i++)
+        {
+            if (s[i] != temp[i])
+            {
+                ans++;
+            }
+        }
+        cout << (ans >= 3 ? "NO" : "YES") << endl;
+    }
+    return 0;
+}
