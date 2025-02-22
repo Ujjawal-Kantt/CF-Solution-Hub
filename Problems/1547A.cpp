351aaed99301ba54214bdc243b451ff24d1db3b1
Ujjawal-Kantt <uk1287@srmist.edu.in>
2025-02-22 21:35:10 +0530

    22-2-25

---
+++ Problems/1547A.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+void solve()
+{
+    int xA, yA, xB, yB, xF, yF;
+    cin >> xA >> yA >> xB >> yB >> xF >> yF;
+
+    int distance = abs(xA - xB) + abs(yA - yB);
+
+    if ((xA == xB && xB == xF && ((yF > yA && yF < yB) || (yF > yB && yF < yA))) ||
+        (yA == yB && yB == yF && ((xF > xA && xF < xB) || (xF > xB && xF < xA))))
+    {
+        distance += 2;
+    }
+
+    cout << distance << "\n";
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
